1811c85f929adcc006f4ffd78a94e7235f072b0c
luail <kynia0506@naver.com>
2025-12-29 22:26:37 +0900

    hello tf

--- main.tf
+++ main.tf
@@ -1,21 +1,15 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
 }
 
-# 외부 입력값으로 terraform은 값을 외부에서 주입받을 수 있다.
-# default는 값이 없을 경우의 default이다.
-variable "env" {
-  type = string
-  default = "dev"
-}
-
-# locals는 Terraform 내부 계산용.
-# 이름 규칙, 태그 규칙 만들 때 핵심이다.
-locals {
-  service_name = "terraform-practice"
-  full_name = "${local.service_name}-${var.env}"
-}
-
-output "full_name" {
-  value = local.full_name
+resource "local_file" "hello" {
+  filename = "hello.txt"
+  content  = "hello terraform"
 }
